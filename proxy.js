678494465582fceff045f52200ef0fc99845aa8f
zhou <806098942@qq.com>
2015-01-11 23:11:30 +0800

    add cmd support

--- proxy.js
+++ proxy.js
@@ -1,1 +1,9 @@
-()
+!(function(name,factory){
+  if(typeof define === 'funciton'){
+    define(facotry)
+  }else if(typeof module !=='undefined' && module.exports){
+     module.exports = factory(require('debug')('touchproxysk'));
+  }else{
+    this.name = factory();
+  }
+}('touchproxysk');
